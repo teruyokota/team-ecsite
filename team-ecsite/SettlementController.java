e4885b06445179d58810c61b1c380abc0d7269b7
teruyokota <yokotateruaki@yokotaakiramei-no-MacBook-Pro.local>
2020-10-02 14:01:59 +0900

    2度目のコミット

---
+++ team-ecsite/SettlementController.java
@@ -0,0 +1,73 @@
+package jp.co.internous.sampleweb.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import jp.co.internous.sampleweb.model.domain.MstDestination;
+import jp.co.internous.sampleweb.model.mapper.MstDestinationMapper;
+import jp.co.internous.sampleweb.model.mapper.TblCartMapper;
+import jp.co.internous.sampleweb.model.mapper.TblPurchaseHistoryMapper;
+import jp.co.internous.sampleweb.model.session.LoginSession;
+
+@Controller
+@RequestMapping("/sampleweb/settlement")
+public class SettlementController {
+	
+	@Autowired
+	MstDestinationMapper destinationMapper;
+	
+	@Autowired
+	LoginSession loginSession;
+	
+	private Gson gson = new Gson();
+	//Gsonの機能とは
+	
+	@Autowired
+	TblCartMapper cartMapper;
+	
+	@Autowired
+	TblPurchaseHistoryMapper purchaseHistoryMapper;
+	
+	@RequestMapping("/")
+	public String index(Model m) {
+		int userId = loginSession.getUserId();
+		
+		List<MstDestination> destinations = destinationMapper.findByUserId(userId);
+		m.addAttribute("destinations", destinations);
+		m.addAttribute("loginSession",loginSession);
+		
+		return "settlement";
+	}
+	
+	@SuppressWarnings("unchecked")
+	@RequestMapping("/complete")
+	@ResponseBody
+	public boolean complete(@RequestBody String destinationId) {
+		
+		Map<String, String> map = gson.fromJson(destinationId, Map.class);
+		String id = map.get("destinationId");
+		
+		int userId = loginSession.getUserId();
+		Map<String, Object> parameter = new HashMap<>();
+		parameter.put("destinationId", id);
+		parameter.put("userId", userId);
+		int insertCount = purchaseHistoryMapper.insert(parameter);
+		
+		int deleteCount = 0;
+		if (insertCount > 0) {
+			deleteCount = cartMapper.deleteByUserId(userId);
+		}
+		return deleteCount == insertCount;
+	}
+
+}
